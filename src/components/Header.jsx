b1886d5a89faf638609e0de8d0bb5e46dcf9a352
P <dpheeraphat2547@gmail.com>
2023-03-07 21:15:38 +0700

    last fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import Nav from "./Nav";
 import Arrow from "../assets/images/icon-arrow-down.svg";
+import "../App.css";
 
 function Header() {
   return (
     <div className="h-[60rem] bg-header bg-cover bg-center text-center">
       <Nav />
       <h1
-        className="text-5xl md:text-6xl lg:text-7xl uppercase text-center text-white mt-10 md:mt-14 "
-        style={{ fontFamily: "Fraunces" }}
+        className="text-5xl mt-32  md:text-6xl lg:text-7xl uppercase text-center text-white"
+        style={{ fontFamily: "Fraunces" }} id="heading"
       >
         We are creatives
       </h1>
-      <div className="flex justify-center mt-16 md:mt-24">
+      <div className="flex justify-center md:mt-24">
         <a href="#about"><img
           src={Arrow}
           alt="arrow"
-          className="mt-[5rem] max-w-sm animate-bounce"
+          className="mt-[15rem] max-w-sm animate-bounce"
         /></a>
         
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,49 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
+import Hamburger from "../assets/images/icon-hamburger.svg";
 
 function Nav() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setscreenWidth] = useState(window.innerWidth);
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setscreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col md:flex-row lg:flex-row justify-between px-[0.5rem] py-[3rem]">
-      <div className="logo mt-2">
+      <div className="logo mt-3 flex ">
         <a
           href="#"
-          className="text-white font-bold text-4xl"
+          className="text-white font-bold text-4xl lg:pl-8"
           style={{ fontFamily: "Fraunces" }}
         >
           sunnyside
         </a>
-      </div>
-      <div className="togmenu">
-        <ul
-          className="menu flex flex-col md:flex-row lg:flex-row items-center text-white"
-          style={{ fontFamily: "Barlow" }}
+
+        <div
+          onClick={toggleNav}
+          className="Hamburger ml-auto order-last mt-3 md:hidden"
         >
-          <li>
-            <a href="#about" className="mx-[20px] my-[10px] text-lg">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#services" className="mx-[20px] my-[10px] text-lg">
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="#projects" className="mx-[20px] my-[10px] text-lg">
-              Projects
-            </a>
-          </li>
-          <li>
-            <a
-              href="#contact"
-              className="mx-[30px] my-[10px] flex justify-center items-center bg-white hover:bg-opacity-50 w-[100px] h-[45px] text-black rounded-full font-bold px-20 uppercase text-lg contact hover:text-white"
-              style={{ fontFamily: "Fraunces" }}
-            >
-              Contact
-            </a>
-          </li>
-        </ul>
+          <img src={Hamburger} alt="togglebutton" />
+        </div>
       </div>
+      {(toggleMenu || screenWidth > 767) && (
+        <div className="togmenu">
+          <ul
+            className="menu flex flex-col md:flex-row lg:flex-row items-center text-white"
+            style={{ fontFamily: "Barlow" }}
+          >
+            <li>
+              <a href="#about" className="mx-[20px] my-[10px] text-lg">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="mx-[20px] my-[10px] text-lg">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#projects" className="mx-[20px] my-[10px] text-lg">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className="mx-[30px] my-[10px] flex justify-center items-center bg-white hover:bg-opacity-50 w-[100px] h-[45px] text-black rounded-full font-bold px-20 uppercase text-lg contact hover:text-white"
+                style={{ fontFamily: "Fraunces" }}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
